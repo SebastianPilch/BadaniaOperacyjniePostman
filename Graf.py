41fe581e124824f81d414613e2a77dbfec9cca3b
SebastianPilch <81679135+SebastianPilch@users.noreply.github.com>
2022-12-12 21:06:53 +0100

    Add files via upload

---
+++ Graf.py
@@ -0,0 +1,134 @@
+class Edge:
+    def __init__(self, Paczkomat1, Paczkomat2, edge=1):
+        self.Paczkomat_out_ = Paczkomat1
+        self.Paczkomat_in_ = Paczkomat2
+        self.time_ = edge
+
+    def __eq__(self, other):
+        if (self.Paczkomat_in_ == other.Paczkomat_in_) and (self.Paczkomat_out_ == other.Paczkomat_out_):
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        return True if self.time_ < other.time_ else False
+
+    def __gt__(self, other):
+        return True if self.time_ > other.time_ else False
+
+    def __le__(self, other):
+        return True if self.time_ <= other.time_ else False
+
+    def __ge__(self, other):
+        return True if self.time_ >= other.time_ else False
+
+    def __str__(self):
+        return '{' + str(self.Paczkomat_out_) + '->' + str(self.Paczkomat_in_) + ',' + str(self.time_) + '}'
+
+
+class MapaPolaczen:
+    def __init__(self):
+        self.PaczkomatList = []
+        self.Dict_ = {}
+        self.PaczkomatDict = {}
+
+    def __str__(self):
+        str_mat = '------GRAPH------,' + str(self.order()) + '\n'
+        for i in self.Dict_.keys():
+            str_mat += '[' + str(i) + ': '
+            for j in range(len(self.Dict_[i])):
+                str_mat += str(self.Dict_[i][j])
+                if j != len(self.Dict_[i]) - 1:
+                    str_mat += ',  '
+            str_mat += ']\n'
+        return str_mat + '-------------------'
+
+    def InsertPaczkomat(self, vertex):
+        """
+        Dodawanie poaczkomatu
+        """
+        self.PaczkomatDict[vertex] = self.order()
+        self.Dict_[vertex] = []
+        self.PaczkomatList.append(vertex)
+
+    def InsertEdges(self, vertex1, vertex2, edge):
+        """
+        Dodawanie krawędzi
+        """
+        idx_1 = self.getPaczkomatIdx(vertex1)
+        idx_2 = self.getPaczkomatIdx(vertex2)
+        self.Dict_[vertex1].append(Edge(self.getPaczkomat(idx_1), self.getPaczkomat(idx_2), edge))
+
+    def deletePaczkomat(self, vertex):
+        """
+        Usuwanie paczkomatu z grafu
+        """
+        v_id = self.getPaczkomatIdx(vertex)
+        del self.Dict_[vertex]
+        self.PaczkomatList.remove(vertex)
+        del self.PaczkomatDict[vertex]
+        for i in self.PaczkomatDict.keys():
+            if self.PaczkomatDict[i] > v_id:
+                self.PaczkomatDict[i] -= 1
+            for j in range(len(self.Dict_[i])):
+                if self.Dict_[i][j].Paczkomat_in_ == vertex:
+                    del self.Dict_[i][j]
+                    break
+
+    def deleteEdge(self, vertex1, vertex2):
+        """
+        Usuwanie krawędzi z grafu
+        """
+        edge = Edge(vertex1, vertex2, None)
+        for i in range(len(self.Dict_[vertex1])):
+            if self.Dict_[vertex1][i] == edge:
+                del self.Dict_[vertex1][i]
+                break
+
+    def getPaczkomatIdx(self, vertex):
+        """
+        Zwraca id paczkomatu po nazwie
+        """
+        return self.PaczkomatDict[vertex]
+
+    def getPaczkomat(self, vertex_id):
+        """
+        Zwraca nazwę paczkomatu po id
+        """
+        return self.PaczkomatList[vertex_id]
+
+    def neighbours(self, vertex_id):
+        """
+        Zwraca sąsiadów paczkomatu w formie paczkomatów
+        """
+        neighbours = []
+        vertex = self.getPaczkomat(vertex_id)
+        for i in self.Dict_.keys():
+            for edge in self.Dict_[i]:
+                if edge.Paczkomat_in_ == vertex and edge.Paczkomat_out_ not in neighbours:
+                    neighbours.append(edge.Paczkomat_out_)
+                if edge.Paczkomat_out_ == vertex and edge.Paczkomat_in_ not in neighbours:
+                    neighbours.append(edge.Paczkomat_in_)
+        return neighbours
+
+    def size(self):
+        """
+        Liczba wzystkich ścieżek między paczkomatami
+        """
+        return len(self.edges())
+
+    def order(self):
+        """
+        Wymiar grafu, liczba wszystkich paczkomatów
+        """
+        return len(self.PaczkomatList)
+
+    def edges(self):
+        """
+        zwraca listę krawędzi
+        """
+        lst = []
+        for i in self.Dict_.keys():
+            for j in range(len(self.Dict_[i])):
+                lst.append(self.Dict_[i][j])
+        return lst
---
+++ Paczka_Paczkomat.py
@@ -0,0 +1,171 @@
+from typing import List
+import random
+import Graf
+import copy
+import math
+from enum import Enum
+
+
+def Funkcja_zysk_szybka(start_val,czas):
+    return start_val * math.exp(-czas/275)
+
+
+def Funkcja_zysk_priorytet(start_val,czas):
+    return start_val * math.exp(-czas / 100)
+
+
+def Funkcja_zysk_Normalna(start_val,czas):
+    return start_val - (czas/70)
+
+
+def Funkcja_zysk_Dlugi_czas(start_val,czas):
+     return (start_val+1)- math.exp(czas/400)
+
+
+class Paczka:
+    def __init__(self,wartosc_poczatkowa, typ ,key, adres_dostarczenia, czas: float,dostarczenie: bool):
+        self.key = key
+        self.adres_dostarczenia = adres_dostarczenia
+        self.dostarczenie = dostarczenie
+        self.wartosc_poczatkowa = wartosc_poczatkowa
+        self.typ = typ
+
+        if typ == 1:
+            self.fun = Funkcja_zysk_Dlugi_czas
+        if typ == 2:
+            self.fun = Funkcja_zysk_Normalna
+        if typ == 3:
+            self.fun = Funkcja_zysk_szybka
+        if typ == 4:
+            self.fun = Funkcja_zysk_priorytet
+
+    def zysk(self):
+        return self.wartosc_poczatkowa
+
+    def strata(self, aktualny_czas):
+        return self.wartosc_poczatkowa - self.bilans(aktualny_czas)
+
+    def bilans(self, aktualny_czas):
+        return self.fun(start_val=self.wartosc_poczatkowa, czas=aktualny_czas)
+
+    def __str__(self):
+        t_ = ''
+        if self.typ == 1:
+            t_ = 'Długi czas doręczenia'
+        if self.typ == 2:
+            t_ = "Normalna"
+        if self.typ == 3:
+            t_ = "Krótki czas doręczenia"
+        if self.typ == 4:
+            t_ = "Priorytetowa"
+
+        return "[" + str(self.key) +',' +str(self.adres_dostarczenia) + "," + t_ + "]"
+
+
+def random_paczka(Kurier, Pacz_lst, liczba_iter, map: Graf.MapaPolaczen, key_Pacz_list: List[int] = [0]):
+
+    for i in range(liczba_iter):
+        Pacz_list = copy.copy(Pacz_lst)
+        key = max(key_Pacz_list) + 1
+        key_Pacz_list.append(key)
+        adres_akt = random.choice(Pacz_list)
+        index = Pacz_list.index(adres_akt)
+        del Pacz_list[index]
+        adres_dos = random.choice(Pacz_list)
+        wartosc = random.randint(0, 480)
+        typ = random.randint(1, 4)
+        if key % 2 == 0:
+            dostarczenie = True
+            paczka_dod = Paczka(wartosc, typ, key, 'Kurier', adres_dos, dostarczenie)
+            Kurier.InsertPaczka(paczka_dod)
+        else:
+            dostarczenie = False
+            paczka_dod = Paczka(wartosc, typ, key, adres_akt, adres_dos, dostarczenie)
+            index = map.getPaczkomatIdx(adres_akt)
+            paczko: Paczkomat = map.getPaczkomat(index)
+            paczko.InsertPaczka(paczka_dod)
+
+class Paczkomat:
+    def __init__(self, adres):
+        self.key_ = adres
+        self.lista_odbior = []
+        self.lista_dostarczenie = []
+
+    def __eq__(self, other):
+        return True if self.key_ == other.key_ else False
+
+    def __hash__(self):
+        return hash(self.key_)
+
+    def __str__(self):
+        return self.key_
+
+    def InsertPaczka(self, Paczka):
+        if Paczka.dostarczenie:
+            self.lista_dostarczenie.append(Paczka)
+        else:
+            self.lista_odbior.append(Paczka)
+
+    def odbior_size(self):
+        return len(self.lista_odbior)
+
+    def dostarczenie_size(self):
+        return len(self.lista_dostarczenie)
+
+    def DeletePaczka(self, Paczka):
+        for i in range(self.odbior_size()):
+            if self.lista_odbior[i].key == Paczka.key:
+                del self.lista_odbior[i]
+        for i in range(self.dostarczenie_size()):
+            if self.lista_dostarczenie[i].key == Paczka.key:
+                del self.lista_dostarczenie[i]
+
+    def zysk(self, time):
+        suma_zysk = 0
+        for i in range(self.odbior_size()):
+            suma_zysk += self.lista_odbior[i].zysk(time)
+        for i in range(self.dostarczenie_size()):
+            suma_zysk += self.lista_dostarczenie[i].zysk(time)
+        return suma_zysk
+
+    def strata(self, time):
+        suma_strat = 0
+        for i in range(self.odbior_size()):
+            suma_strat += self.lista_odbior[i].strata(time)
+        for i in range(self.dostarczenie_size()):
+            suma_strat += self.lista_dostarczenie[i].strata(time)
+        return suma_strat
+
+    def bilans(self, time):
+        return self.zysk(time) - self.strata(time)
+
+    def Print_zawartosc(self):
+        str_ = str(self) + ":"
+        for i in self.lista_odbior:
+            str_ += " " + str(i) + " "
+        for i in self.lista_dostarczenie:
+            str_ += " " + str(i) + " "
+        print(str_)
+
+class Kurier:
+    def __init__(self):
+        self.Paczki = []
+
+    def __str__(self):
+        to_str = 'Kurier : '
+        for i in range(len(self.Paczki)):
+            to_str += f'[{self.Paczki[i].adres_dostarczenia} : {self.Paczki[i].key}]'
+        return to_str
+
+    def InsertPaczka(self, Paczka):
+        self.Paczki.append(Paczka)
+
+    def Dostarczenie(self, Paczkomat):
+        d = 0
+        for i in range(len(self.Paczki)):
+            if Paczkomat.key_ == self.Paczki[i-d].adres_dostarczenia:
+                Paczkomat.InsertPaczka(self.Paczki[i - d])
+                del self.Paczki[i-d]
+                d += 1
+
+
